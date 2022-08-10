1a27015963aad28a4db9844cf91a016959e39ddb
Юлия Кудрова <bahtina_yul@mail.ru>
2022-08-10 20:30:37 +0800

    Slider refinement

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,10 +3,8 @@ const itemSlide = document.querySelector('.slider__items').children;
 
 showSlides(slideIndex);
 
-
 document.querySelector('div.slider__arrow_next').addEventListener('click', nextSlide);
 document.querySelector('div.slider__arrow_prev').addEventListener('click', previousSlide);
-
 
 function nextSlide() {
 	showSlides(slideIndex += 1);
@@ -21,24 +19,22 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-	let i;
-	let slides = document.getElementsByClassName('slider__item')//.classList.add('slider__item_active');
+	let slides = document.getElementsByClassName('slider__item');
+	slides[0].classList.add('slider__item_active');
 
-slides[0].classList.add('slider__item_active');
+		if (n > slides.length) {
+			slideIndex = 1;
+		}
 
-if (n > slides.length) {
-	slideIndex = 1;
-}
+		if (n < 1) {
+			slideIndex = slides.length;
+		}
 
-if (n < 1) {
-	slideIndex = slides.length;
-}
+		for (let i=0; i < slides.length; i++) {
+			slides[i].classList = 'slider__item';
+		}
 
-for (let i=0; i < slides.length; i++) {
-	slides[i].style.display = 'none';
-}
-
-	slides[slideIndex - 1].style.display = 'block';
+		slides[slideIndex - 1].classList.toggle('slider__item_active');
 }
 
 
