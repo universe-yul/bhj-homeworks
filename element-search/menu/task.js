c3efafb047aa862c53ed9b0afc878b7a5e1e9010
Юлия Кудрова <bahtina_yul@mail.ru>
2022-08-10 20:27:20 +0800

    Menu refinement

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,10 +1,17 @@
-const menuChild = document.querySelector('.menu').children; 
-  for (let i = 1; i < menuChild.length-1 ; i++) {
-	   menuChild[i].onclick = function() {
-	   this.querySelector('ul').classList.toggle('menu_active');
-	
-   			return false 
-   	 }
-  }
+const links = document.querySelectorAll('.menu__link');
 
+links.forEach((link) => {
+	link.onclick = function (event) {
+		const element = document.querySelector('.menu_active')
+
+		if (element) {
+			element.classList.toogle('menu_active');
+		}
+		if (link.closest('.menu__item').querySelector('.menu_sub')) {
+			link.closest('.menu__item').querySelector('.menu_sub').classList.toggle('menu_active')
+			
+			return false;
+		}
+	}
+})
 
