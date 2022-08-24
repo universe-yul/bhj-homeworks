cc85b9932aaaa941885ea9fd9a906729574fb683
Юлия Кудрова <bahtina_yul@mail.ru>
2022-08-24 16:07:26 +0800

    Added homework autocomplete

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -67,8 +67,18 @@ class Autocomplete {
     this.list.innerHTML = html.join('');
   }
 
+
   getMatches( text ) {
-    /*
+    const arrWords = [];
+    for (let item of this.input.options) {
+      if (item.text.includes(text)){
+      arrWords.push({
+        text: item.text,
+        value: item.value
+      })
+    }
+  }
+  /*
       TODO: этот метод нужно дописать
       text - фраза, которую вводят в поле поиска
       Метод должен вернуть массив.
@@ -81,12 +91,7 @@ class Autocomplete {
         value: 'Содержимое атрибута value'
       }
     */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    return arrWords;
   }
 }
 
